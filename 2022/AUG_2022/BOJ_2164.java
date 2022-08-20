6cbbf6c654f09082399df6ccef79226c14fe987f
kong0527 <o____ri@naver.com>
2022-08-20 17:44:33 +0900

    solve BOJ_2164

---
+++ 2022/AUG_2022/BOJ_2164.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static int solution(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() != 1) {
+            queue.poll();
+
+            if (queue.size() > 1) {
+                int num = queue.poll();
+                queue.add(num);
+            }
+        }
+
+        return queue.poll();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(solution(n));
+    }
+}
