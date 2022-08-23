0202b6b0f8e39241dad53642173f2889a8593a86
kong0527 <o____ri@naver.com>
2022-08-23 15:52:43 +0900

    solve BOJ_11399

---
+++ 2022/AUG_2022/BOJ_11399.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int solution(int[] arr) {
+        // 삽입정렬로 정렬
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                }
+            }
+        }
+
+        int answer = arr[0];
+        int prev = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            prev += arr[i];
+            answer += prev;
+        }
+
+        return answer;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(arr));
+    }
+}
