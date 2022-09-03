367169d8a860ac5156b3f062a5042ed51a47b81e
kong0527 <o____ri@naver.com>
2022-09-03 23:32:48 +0900

    solve BOJ_11047

---
+++ 2022/SEP_2022/BOJ_11047.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] > k) {
+                continue;
+            }
+
+            int divide = k / arr[i];
+            int mod = k % arr[i];
+            cnt += divide;
+            k = mod;
+        }
+
+        System.out.println(cnt);
+    }
+}
